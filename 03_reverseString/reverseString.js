fb7a5eb5d040492683644426118bbe1750ec3a60
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 06:46:01 +0700

    Remove for loop, use .toReversed method to reverse list, join reversed list and assign to newString variable

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,13 @@
 const reverseString = function(string) {
     let stringList = string.split('');
     let reversedList = [];
-    let newString = '';
+    let newString;
 
-    for (let i = 0; i < stringList.length; i++) {
-        reversedList.push(stringList[stringList - 1]);
-    }
+    reversedList = stringList.toReversed();
 
-    reversedList.toString();
+    newString = reversedList.join('');
 
-    return stringList;
+    return newString;
 };
 
 // Do not edit below this line
