920748842eae5df5c0234ae482646ac76e31963f
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 06:21:07 +0700

    Create variables to store split string and empty list, use for loop to iterate through list and push last character

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringList = string.split();
+    let reversedList = [];
 
+    for (let i = 0; i < stringList.length; i++) {
+        reversedList.push(stringList[stringList - 1]);
+    }
+
+    return reversedList;
 };
 
 // Do not edit below this line
