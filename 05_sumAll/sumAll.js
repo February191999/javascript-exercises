92c78aa5822a754d82faf7ccae1d376bc9d8de8f
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 08:55:57 +0700

    Add condition to check if parameters are integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(firstNumber, secondNumber) {
     let addedSum = 0;
     let i = firstNumber;
 
-    if (firstNumber < 0) {
+    if (firstNumber < 0 || Number.isInteger(firstNumber) === false || Number.isInteger(secondNumber) === false ) {
         return "ERROR"
     } else if (firstNumber < secondNumber) {
         while (i <= secondNumber) {
