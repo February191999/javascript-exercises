392835f457f791c5cf0f1be654b4cab6abfbfef9
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 08:16:59 +0700

    Create new duplicateArray, use for loop to loop through args, filter unwanted values and assign to duplicateArray after each iteration

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,26 @@
 const removeFromArray = function(array, ...theArgs) {
+    let duplicateArray = array;
     let newArray = [];
 
-    for (const arg of theArgs) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === arg) {
-                continue;
-            }   else {
-                newArray.push(array[i]);
-            }
-        }
+
+    // for (const arg of theArgs) {
+    //     for (let i = 0; i < array.length; i++) {
+            
+    //         let checkForDuplicate = newArray.find(({element}) => element === arg);
+
+    //         if (array[i] !== checkForDuplicate || array[i] !== arg) {
+    //             newArray.push(array[i]);
+    //         } else {
+    //             continue;
+    //         }
+    //     }
+    // }
+
+    for (let arg of theArgs) {
+        duplicateArray = duplicateArray.filter((unwanted) => unwanted !== arg);
     }
+
+    newArray = duplicateArray
 
     return newArray;
 
