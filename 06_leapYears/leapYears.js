3b8efdedd86d3eec45062959662988f4182b8386
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 09:29:28 +0700

    Add if condition to check for years divisible by 100 and 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
 const leapYears = function(year) {
-    if (year % 4 === 0 && year % 400) {
+    // if (year % 4 === 0 || year % 100 === 0 && year % 400 === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (year % 4 === 0) {
         return true;
     } else {
         return false;
