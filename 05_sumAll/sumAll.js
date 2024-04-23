a51fcd9f5befd262dba4bd943f5de28489d2d928
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 08:49:53 +0700

    Add if and else if condition to check if firstNumber is bigger or smaller than secondNumber

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,9 +2,16 @@ const sumAll = function(firstNumber, secondNumber) {
     let addedSum = 0;
     let i = firstNumber;
 
-    while (i <= secondNumber) {
-        addedSum += i;
-        i++
+    if (firstNumber < secondNumber) {
+        while (i <= secondNumber) {
+            addedSum += i;
+            i++
+        }
+    } else if (firstNumber > secondNumber) {
+        while (i >= secondNumber) {
+            addedSum += i;
+            i--;
+        }
     }
 
     return addedSum;
