3f396c4a29c30f464b41efccc3c20cce69fc9477
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-23 08:19:57 +0700

    Remove old code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,20 +2,6 @@ const removeFromArray = function(array, ...theArgs) {
     let duplicateArray = array;
     let newArray = [];
 
-
-    // for (const arg of theArgs) {
-    //     for (let i = 0; i < array.length; i++) {
-            
-    //         let checkForDuplicate = newArray.find(({element}) => element === arg);
-
-    //         if (array[i] !== checkForDuplicate || array[i] !== arg) {
-    //             newArray.push(array[i]);
-    //         } else {
-    //             continue;
-    //         }
-    //     }
-    // }
-
     for (let arg of theArgs) {
         duplicateArray = duplicateArray.filter((unwanted) => unwanted !== arg);
     }
