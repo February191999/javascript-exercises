ff9b72d0fce45614b8e9c2dd4284656d55cadc1e
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-30 06:53:12 +0700

    Create variables to store split words and joined words, create if condition to return true if new joined words are equal

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringArray = string.split("");
+    let stringToTest = stringArray.join("");
+
+    let reversedString = stringArray.reverse();
+    let reversedStringToTest = reversedString.join("");
+
+    if (stringToTest === reversedStringToTest) {
+        return true;
+    }
 
 };
 
