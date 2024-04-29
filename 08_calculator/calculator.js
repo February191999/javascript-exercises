44997875104a0560779d4518be2c71672b4de04a
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-30 05:25:27 +0700

    Use reduce to add current item to total for each item in array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,10 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, currentNum) => {
+    total + currentNum;
+  }, 0);
 };
 
 const multiply = function() {
