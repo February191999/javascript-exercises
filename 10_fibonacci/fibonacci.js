1d046d50c0fd9bbdcca6b73e88bbe37afe57035f
February191999 <96108108+February191999@users.noreply.github.com>
2024-05-01 02:11:57 +0700

    Add if statement to return 1 if num is 1 or 2

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -5,9 +5,7 @@ const fibonacci = function(num) {
 
     if (num === 0) {
         return 0;
-    } else if (num === 1) {
-        return 1;
-    } else if (num === 2) {
+    } else if (num === 1 || num === 2) {
         return 1;
     }
 
