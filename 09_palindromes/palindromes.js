cf23b981d0a5314ddf956796f03be76747c2663b
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-30 07:02:59 +0700

    Add filter function to filter string for !

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,17 @@
 const palindromes = function (string) {
     let stringArray = string.split("");
-    let stringToTest = stringArray.join("");
+    let stringToTest = stringArray
+        .filter(char => char !== "!")
+        .join("");
 
-    let reversedString = stringArray.reverse();
-    let reversedStringToTest = reversedString.join("");
+    return stringToTest;
 
-    if (stringToTest === reversedStringToTest) {
-        return true;
-    }
+    // let reversedString = stringArray.reverse();
+    // let reversedStringToTest = reversedString.join("");
+
+    // if (stringToTest === reversedStringToTest) {
+    //     return true;
+    // }
 
 };
 
