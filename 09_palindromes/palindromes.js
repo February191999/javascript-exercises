e73ffce463e3d95fe336018a8d8c4716c6e8c899
February191999 <96108108+February191999@users.noreply.github.com>
2024-05-01 02:11:05 +0700

    Create numArray to contain array of fib numbers, use if statements to return 0 if num equals to 0

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,32 +1,44 @@
 const palindromes = function (string) {
-    let stringArray = string.split("");
+    // let stringArray = string.split("");
     
-    let filterExclamation = stringArray
-        .filter(char => char !== "!")
-        .map(char => char.toLowerCase());
+    // let filterExclamation = stringArray
+    //     .filter(char => char !== "!")
+    //     .map(char => char.toLowerCase());
 
-    let filterSpace = filterExclamation
-        .filter(char => char !== " ");
+    // let filterSpace = filterExclamation
+    //     .filter(char => char !== " ");
 
-    let filterComma = filterSpace
-        .filter(char => char !== ",");
+    // let filterComma = filterSpace
+    //     .filter(char => char !== ",");
 
-    let filterPeriod = filterComma  
-        .filter(char => char !== ".");
+    // let filterPeriod = filterComma  
+    //     .filter(char => char !== ".");
 
-    let stringToTest = filterPeriod.join("");
+    // let stringToTest = filterPeriod.join("");
 
-    let reversedString = filterPeriod.reverse();
-    let reversedStringToTest = reversedString
-        .filter(char => char !== "!")
-        .map(char => char.toLowerCase())
-        .join("");
+    // let reversedString = filterPeriod.reverse();
+    // let reversedStringToTest = reversedString
+    //     .filter(char => char !== "!")
+    //     .map(char => char.toLowerCase())
+    //     .join("");
 
-    if (stringToTest !== reversedStringToTest) {
-        return false;
-    } else {
-        return true;
-    }
+    // if (stringToTest !== reversedStringToTest) {
+    //     return false;
+    // } else {
+    //     return true;
+    // }
+
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    let numArray = [0, 1];
+    let checkDifference = num - numArray.length;
 
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else if (num === 2) {
+        return 1;
+    }
+
+    for (let i = 1; i < checkDifference + 2; i ++) {
+        let newFibNumber = numArray[i] + numArray[(i-1)];
+        numArray.push(newFibNumber);
+    }
+
+    return numArray.pop();
 };
 
 // Do not edit below this line
