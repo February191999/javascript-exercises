40dcf0f7761772448b3eb2e8fa0b2bb7331148bd
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-30 08:10:52 +0700

    Create variables to store filtered arrays

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,22 @@
 const palindromes = function (string) {
     let stringArray = string.split("");
-    let stringToTest = stringArray
+    
+    let filterExclamation = stringArray
         .filter(char => char !== "!")
-        .map(char => char.toLowerCase())
-        .join("");
+        .map(char => char.toLowerCase());
 
-    let reversedString = stringArray.reverse();
+    let filterSpace = filterExclamation
+        .filter(char => char !== " ");
+
+    let filterComma = filterSpace
+        .filter(char => char !== ",");
+
+    let filterPeriod = filterComma  
+        .filter(char => char !== ".");
+
+    let stringToTest = filterPeriod.join("");
+
+    let reversedString = filterPeriod.reverse();
     let reversedStringToTest = reversedString
         .filter(char => char !== "!")
         .map(char => char.toLowerCase())
@@ -14,7 +25,6 @@ const palindromes = function (string) {
     if (stringToTest === reversedStringToTest) {
         return true;
     }
-
 };
 
 // Do not edit below this line
