c225ff2e1b6ffb1dd6900545cafa710c27a44fe8
February191999 <96108108+February191999@users.noreply.github.com>
2024-05-02 22:10:04 +0700

    Create function called getAge to calculate age using deathYear and birthYear, assigns current year to deathYear if deathYear is undefined

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
 
+    return people.reduce((previousPerson, currentPerson) => {
+        let currentAge = person.yearOfDeath-person.yearOfDeathBirth;
+
+        if (currentAge > previousAge) {
+            return 1;
+        } else {
+            return -1;
+        }
+
+    }, {});
 };
+
+function getAge(birthYear, deathYear) {
+
+    if (deathYear === undefined) {
+        deathYear = new Date().getFullYear();
+    } 
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
